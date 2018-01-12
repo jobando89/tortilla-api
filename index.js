cc965c62d5664bdd731dd13561ac8a07ae17c39b
Joel Obando <j-obando@live.com>
2018-01-12 10:02:48 -0500

    Adding a different start file

--- index.js
+++ index.js
@@ -3,7 +3,3 @@ const server = require('./src/server');
 module.exports = {
     create: server.create
 };
-
-server.create({
-    appRoot: __dirname
-});
---
+++ start.js
@@ -0,0 +1,6 @@
+const server = require('./src/server');
+
+
+server.create({
+    appRoot: __dirname
+});
